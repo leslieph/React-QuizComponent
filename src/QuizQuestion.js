e7ec43228bff0ccec9737952769bd59a104af363
Leslie Phifer <lphifer@nextekdevelopment.com>
2019-03-20 14:26:46 -0500

    Add QuizQuestion component

---
+++ src/QuizQuestion.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+class QuizQuestion extends Component {
+  render() {
+    //let {answer_options, instruction_text} = this.props.quiz_question;
+
+    return (
+      <article>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+          </ul>
+        </section>
+      </article>
+    );
+  }
+}
+
+export default QuizQuestion;
