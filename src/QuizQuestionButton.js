35222c863fcad3ac8bf22f078bfd32da62af88fb
Leslie Phifer <lphifer@nextekdevelopment.com>
2019-03-20 14:53:57 -0500

    Re-structure button component

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+<<<<<<< Updated upstream
 
 export default class QuizQuestionButton extends Component {
+=======
+
+class QuizQuestionButton extends Component {
+>>>>>>> Stashed changes
   render(){
     return (
       <li><button>{this.props.button_text}</button></li>
     );
   }
+<<<<<<< Updated upstream
 }
+=======
+}
+
+export default QuizQuestionButton;
+>>>>>>> Stashed changes
