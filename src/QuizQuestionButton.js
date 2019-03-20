5df5991deb4173adfc0efab1601f398a765fe685
Leslie Phifer <lphifer@nextekdevelopment.com>
2019-03-20 14:55:47 -0500

    Merge fix

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-<<<<<<< Updated upstream
-
-export default class QuizQuestionButton extends Component {
-=======
 
 class QuizQuestionButton extends Component {
->>>>>>> Stashed changes
   render(){
     return (
       <li><button>{this.props.button_text}</button></li>
     );
   }
-<<<<<<< Updated upstream
-}
-=======
 }
 
 export default QuizQuestionButton;
->>>>>>> Stashed changes
