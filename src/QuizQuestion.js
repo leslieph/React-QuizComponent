aaa4f75a565480a8c77dbf337808c7e53c29ed27
Leslie Phifer <lphifer@nextekdevelopment.com>
2019-03-20 16:43:39 -0500

    Add HandleClick functionality to question answers

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,17 +2,25 @@ import React, {Component} from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
-  render() {
-    //let {answer_options, instruction_text} = this.props.quiz_question;
+  handleClick(buttonText){
+    if(buttonText === this.props.quiz_question.answer){
+      this.props.showNextQuestionHandler();
+    }
+  }
 
+  render() {
     return (
       <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
-          <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+          <ul>{
+            this.props.quiz_question.answer_options.map((answer_option, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_option}
+              clickHandler={this.handleClick.bind(this)}/>
+            })
+          }
           </ul>
         </section>
       </main>
